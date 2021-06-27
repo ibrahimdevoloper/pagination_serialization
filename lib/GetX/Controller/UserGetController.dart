1e66be68b7ea3f13dbcc08d78cb6de4fcf007fd9
ibrahimdevoloper <ibrahimdevoloper@gmail.com>
2021-06-27 18:43:23 +0200

    final product

--- lib/GetX/Controller/UserGetController.dart
+++ lib/GetX/Controller/UserGetController.dart
@@ -1,35 +1,83 @@
 import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/src/core/paging_controller.dart';
 import 'package:pagination_serialization/GetX/Services/UserService.dart';
 import 'package:pagination_serialization/Model/User/User.dart';
 
 class UserGetController extends GetxController {
   var isLoading = false;
-  List<User> users = [];
+  // List<User> users = [];
   var isError = false;
+  PagingController<int, User> pagingController;
+
+  UserGetController(this.pagingController) {
+    pagingController.addPageRequestListener((pageKey) {
+      print(pageKey);
+      getUsersPage(pageKey);
+    });
+  }
+  //
+  // @override
+  // void onInit() {
+  //   getUsers();
+  // }
 
   @override
-  void onInit() {
-    getUsers();
+  void dispose() {
+    pagingController.dispose();
   }
 
-  getUsers() async {
+  // getUsers() async {
+  //   users = [];
+  //   isLoading = true;
+  //   // update();
+  //   try {
+  //     var response = await UserService().getUsers();
+  //     print(response);
+  //     response.body.forEach((element) {
+  //       print(User.fromJson(element));
+  //       users.add(User.fromJson(element));
+  //     });
+  //     pagingController.appendPage(users, 0);
+  //     isLoading = false;
+  //     // update();
+  //   } catch (e) {
+  //     // TODO: catch Error
+  //     print(e);
+  //     isLoading = false;
+  //     isError = true;
+  //     pagingController.error = e;
+  //     // update();
+  //   }
+  // }
+
+  getUsersPage(int pageNumber) async {
+    // print(pageNumber);
     isLoading = true;
-    update();
+    // update();
     try {
       var response = await UserService().getUsers();
-      print(response);
-      response.body.forEach((element) {
-        print(User.fromJson(element));
-        users.add(User.fromJson(element));
-      });
+      // print(response);
+      List<User> users = [];
+      if (pageNumber == 0) {
+        response.body.forEach((element) {
+          // print(User.fromJson(element));
+          users.add(User.fromJson(element));
+        });
+        pagingController.appendPage(users, pageNumber + users.length);
+      } else {
+        users = [];
+        pagingController.appendLastPage(users);
+      }
+
       isLoading = false;
-      update();
+      // update();
     } catch (e) {
       // TODO: catch Error
       print(e);
       isLoading = false;
       isError = true;
-      update();
+      pagingController.error = e;
+      // update();
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:pagination_serialization/GetX/Controller/UserGetController.dart';
+import 'package:pagination_serialization/Model/User/User.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,61 +23,70 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final UserGetController userGetController = Get.put(UserGetController());
+    PagingController<int, User> _pagingController = PagingController<int, User>(
+      firstPageKey: 0,
+    );
+    final UserGetController userGetController =
+        Get.put(UserGetController(_pagingController));
     return Scaffold(
+      appBar: AppBar(),
       body: GetBuilder<UserGetController>(builder: (controller) {
-        if (controller.isLoading)
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        else if (controller.isError)
-          return Center(
-            child: Text("error"),
-          );
-        else {
-          return ListView.builder(
-              itemCount: controller.users.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(controller.users[index].name),
-                  subtitle: Text(controller.users[index].address.city),
-                );
-              });
-        }
+        // if (controller.isLoading)
+        //   return Center(
+        //     child: CircularProgressIndicator(),
+        //   );
+        // else if (controller.isError)
+        //   return Center(
+        //     child: Text("error"),
+        //   );
+        // else {
+        return RefreshIndicator(
+          onRefresh: () => controller.getUsersPage(0),
+          child: PagedListView<int, User>(
+            pagingController: controller.pagingController,
+            builderDelegate: PagedChildBuilderDelegate<User>(
+              itemBuilder: (context, user, index) => ListTile(
+                title: Text(user.name),
+              ),
+              firstPageErrorIndicatorBuilder: (context) => ErrorIndicator(),
+              noItemsFoundIndicatorBuilder: (context) => EmptyListIndicator(),
+            ),
+          ),
+        );
+        // }
       }),
     );
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key? key, required this.title}) : super(key: key);
-//   final String title;
-//
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+class ErrorIndicator extends StatelessWidget {
+  const ErrorIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Image.asset("assets/error.png"),
+      ),
+    );
+  }
+}
+
+class EmptyListIndicator extends StatelessWidget {
+  const EmptyListIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Image.asset("assets/box.png"),
+      ),
+    );
+  }
+}
